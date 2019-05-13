b47920e1dfa6d43f62110cb3cf7477629e971661
jdc5247 <jdc5247@msia423.analytics.northwestern.edu>
2019-05-13 15:58:35 -0500

    moving fetch_data script and re-formatting

---
+++ src/fetch_data.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import boto3
+import os
+import logging
+import time
+
+# Set up Logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Set up Boto3
+s3 = boto3.client('s3')
+
+# API Settings
+payload = "{\n \"dappName\": \"KittyFarm\"\n}"
+headers = {
+        'x-api-token': '07HVm340iA9pO8iu0I3lMMniT21DaNZPVU6chOE7nD8'}
+
+# Get All Kitties
+url = 'https://public.api.cryptokitties.co/v1/kitties'
+logger.info("calling CryptoKitties API - getAllKitties")
+
+# initial call to get total
+response = requests.request('GET', url, headers = headers,
+       data = payload, allow_redirects=False)
+logger.info("response status - {}".format(response.status_code))
+result = json.loads(response.text)
+total = result["total"]
+
+# start with offset = 0, increment by 5000, get all kitties
+offset = 0
+increment = 5000
+cntr = 1
+
+while offset < total:
+
+    url = 'https://public.api.cryptokitties.co/v1/kitties?' + "offset=" + str(offset) + "&limit=" + str(increment)
+    logger.info("calling CryptoKitties API - getAllKitties")
+    response = requests.request('GET', url, headers = headers,
+           data = payload, allow_redirects=False)
+    logger.info("response status - {}".format(response.status_code))
+
+    result = json.loads(response.text)
+    out = json.dumps(result,indent=2)
+    key = "kitties" + str(cntr) + ".json"
+
+    s3.put_object(Bucket='jdc-nu',Key=key,Body=out)
+
+    offset = offset + increment
+    logger.info("finished call # " + str(cntr))
+    cntr = cntr + 1
+    time.sleep(.01)
+
+logger.info("FINISHED ALL KITTIES PROCESS")
+
+
+
+

53ec26fc45e1cb0a5a41d81caab960c23f7fe87a
JD <josephcook36@gmail.com>
2019-06-07 15:11:08 -0500

    cleaning up files in repo

--- config/flask_config.py
+++ config/flask_config.py
@@ -1,3 +1,5 @@
+ENV = "dev"
+
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 3000
@@ -10,3 +12,10 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "127.0.0.1"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
+
+if(ENV == "dev"):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../data/kitties.db'
+    HOME_ENGINE_STRING = 'sqlite:///data/kitties.db'
+# else:
+#     # SQLALCHEMY_DATABASE_URI = rds uri here
+#     # HOME_ENGINE_STRING = rds something here

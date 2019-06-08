f4b5f49c9fcb5249eafc9d30889cb9aa93a79993
jdc5247 <jdc5247@msia423.analytics.northwestern.edu>
2019-06-08 15:52:26 -0500

    updates to config

--- config/flask_config.py
+++ config/flask_config.py
@@ -1,6 +1,6 @@
 import os
 
-ENV = "dev"
+ENV = "staging"
 
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
@@ -19,5 +19,5 @@ if(ENV == "dev"):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../data/kitties.db'
     HOME_ENGINE_STRING = 'sqlite:///data/kitties.db'
 else:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com/kittyrds'.format(os.environ["RDS_PASSWORD"])
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com/kittyrds'.format(os.environ["RDS_PASSWORD"])
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com:3306/kittyrds'.format(os.environ["RDS_PASSWORD"])
+    HOME_ENGINE_STRING = 'mysql+pymysql://root:{}@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com:3306/kittyrds'.format(os.environ.get("RDS_PASSWORD"))

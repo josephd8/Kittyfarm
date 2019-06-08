17fb45388449ac07b45fb412c0d0f77fbf84ee76
JD <josephcook36@gmail.com>
2019-06-08 15:29:57 -0500

    updating RDS URI

--- config/flask_config.py
+++ config/flask_config.py
@@ -19,5 +19,5 @@ if(ENV == "dev"):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../data/kitties.db'
     HOME_ENGINE_STRING = 'sqlite:///data/kitties.db'
 else:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:' + os.environ["RDS_PASSWORD"]+ '@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com/kittyrds'
-    HOME_ENGINE_STRING = 'mysql+pymysql://root:' + os.environ["RDS_PASSWORD"]+ '@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com/kittyrds'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com/kittyrds'.format(os.environ["RDS_PASSWORD"])
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:{}@kittyrds.caso2ns6uz08.us-east-2.rds.amazonaws.com/kittyrds'.format(os.environ["RDS_PASSWORD"])
